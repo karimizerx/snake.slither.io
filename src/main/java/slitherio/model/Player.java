e4558d1a9af2dba2fc2af48f6bb3541e123ad3f4
kmzx <mizerxkarim@gmail.com>
2023-12-29 12:13:25 +0100

    [Player]: 'boolean isBot' updated -> isIa

--- src/main/java/slitherio/model/Player.java
+++ src/main/java/slitherio/model/Player.java
@@ -6,13 +6,13 @@ public final class Player {
 
     private int id;
     private String name;
-    private boolean isBot;
+    private boolean isIa;
     private Snake snake;
 
-    public Player(int id, String name, boolean isBot, double x, double y) {
+    public Player(int id, String name, boolean isIa, double x, double y) {
         this.id = id;
         this.name = name;
-        this.isBot = isBot;
+        this.isIa = isIa;
         this.snake = new Snake(x, y);
     }
 
@@ -34,8 +34,8 @@ public final class Player {
         this.name = name;
     }
 
-    public final boolean isBot() {
-        return isBot;
+    public final boolean isIa() {
+        return isIa;
     }
 
     public final Snake getSnake() {

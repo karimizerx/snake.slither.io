fc1fd798469fe886c5b0e6a108bb7d6e3f4d2f79
laidouni <mohamed.laidouni@etu.u-paris.fr>
2024-01-14 15:31:00 +0100

    App documentation added

--- src/main/java/slitherio/App.java
+++ src/main/java/slitherio/App.java
@@ -3,6 +3,7 @@ package slitherio;
 import javafx.application.*;
 import slitherio.view.*;
 
+/** The main class that launches the game. */
 public class App {
 
     public static void main(String[] args) {

82fbe11f80376c5bdc3061331980ab771b92683f
laidouni <mohamed.laidouni@etu.u-paris.fr>
2024-01-14 15:14:42 +0100

    FoodView and SegmentView documentation added

--- src/main/java/slitherio/view/FoodView.java
+++ src/main/java/slitherio/view/FoodView.java
@@ -2,6 +2,7 @@ package slitherio.view;
 
 import slitherio.gameobjects.*;
 
+/** Représente la vue d'un objet {@link slitherio.gameobjects.Food food}. */
 public class FoodView extends DisplayableObject {
 
     protected FoodView(GameObject go) {
--- src/main/java/slitherio/view/SegmentView.java
+++ src/main/java/slitherio/view/SegmentView.java
@@ -2,6 +2,7 @@ package slitherio.view;
 
 import slitherio.gameobjects.*;
 
+/** Représente la vue d'un objet {@link slitherio.gameobjects.Segment segment}. */
 public class SegmentView extends DisplayableObject {
 
     public SegmentView(GameObject go, String file) {
